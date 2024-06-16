bca5af75375e8cdda29ccad1bc1714a5133a3ed8
angusgee <maltawebdev@gmail.com>
2024-06-16 13:15:31 +0200

    Add sanity client and page func

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { groq } from "next-sanity";
+import { client } from "@/sanity/lib/client";
 
-const page = () => {
-  return <div>page</div>;
+const query = groq`
+*[_type == "post"] {
+  ...,
+  author ->,
+} | order(_createdAt, desc)
+`;
+
+const page = async () => {
+  const posts = await client.fetch(query);
+  console.log("Posts", posts);
+  return (
+    <div>
+      {posts.map((post) => (
+        <p>{post.title}</p>
+      ))}
+    </div>
+  );
 };
 
 export default page;
